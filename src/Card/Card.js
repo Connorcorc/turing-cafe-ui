d450a76ba4b3e4ca98d47c4828de39d565dde395
Connor Corcoran <connorcorc@gmail.com>
2022-10-31 10:04:28 -0600

    Add complete card component

---
+++ src/Card/Card.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Card.css'
+
+const Card = ({name, date, time, guests, id, deleteReservation}) => {
+  return (
+    <div className='card'>
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>{guests}</p>
+      <button onClick={() => deleteReservation(id)}>Cancel</button>
+    </div>
+  )
+}
+
+export default Card
