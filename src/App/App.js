abb3052272962267b81c45e45891bfacedbd2348
Connor Corcoran <connorcorc@gmail.com>
2022-10-31 09:39:48 -0600

    Add component files

--- src/App/App.js
+++ src/App/App.js
@@ -1,7 +1,19 @@
 import React, { Component } from 'react';
+// import Reservation from '..Reservation/Reservation.js'
+// import Form from '..Form/Form.js'
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super()
+    this.state = {
+      reservations: []
+    }
+  }
+
+  addReservation = (newReservation) => {
+    this.setState({reservations: [...this.state.ideas, newReservation]})
+  }
   render() {
     return (
       <div className="App">
@@ -10,7 +22,7 @@ class App extends Component {
 
         </div>
         <div className='resy-container'>
-          
+
         </div>
       </div>
     )
